46db10a3925172698fbe1e3c7403e1caae939803
rio_ba <rio.aditya@dp.dharmap.com>
2023-06-21 22:55:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0284c7',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        prime: '#0284c7',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
